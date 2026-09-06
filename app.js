eeb8121ee4d97cf8a35194808d05459444997c25
unknown <richardjose1252@gmai.com>
2026-09-06 14:26:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+
+  res.end(
+    JSON.stringify({
+      message: "Hello from Node.js!",
+      status: "success",
+    })
+  );
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
